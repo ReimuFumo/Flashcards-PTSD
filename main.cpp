003e68afcf91f2a056528575c4d84dac9bcbd060
james <james.lee@student.adelaide.edu.au>
2025-10-17 13:45:40 +1030

    Implemented the insertFlashcards() function. Splits file line into question and answer and stores into vector as pair. Give users choice to revise currently inserted file or return to the start.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <utility>
 
 class Flashcards
 {
@@ -17,6 +19,7 @@ class Flashcards
 
     private:
     void insertFlashcards(const std::string& filename);
+    void beginRevision();
 };
 
 // The welcome page of the Flashcards Application
@@ -24,6 +27,9 @@ void Flashcards::welcome()
 {
     std::string action;
 
+    flashcard.clear(); // Prevents duplication of flashcards when inserting from file
+
+    std::cout << "***************************\n";
     std::cout << "Welcome to PTSD Flashcards!" << "\n";
     std::cout << "Make sure your flashcards file is inside the current directory." << "\n";
     std::cout << "Enter 'select' to choose which file you want to revise or 'q' to exit the app." << "\n\n";
@@ -87,19 +93,49 @@ void Flashcards::selectFile()
             if (!file) std::cout << "File \"" << filename << "\" was not found. Please enter a valid file name.\n";
             else
             {
-                std::cout << "File loaded successfully!\n";
+                std::cout << "File loaded successfully!\n\n";
                 insertFlashcards(filename);
                 break;
             }
         }
     }
-
-    insertFlashcards(filename);
 }
 
 void Flashcards::insertFlashcards(const std::string& filename)
 {
-    return; // NOT IMPLEMENTED YET
+    std::ifstream file(filename);
+    std::string line;
+    std::string action;
+
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        
+        if (std::getline(ss, currQuestion, ';') && std::getline(ss, currAnswer))
+        {
+            if (!currQuestion.empty() && !currAnswer.empty()) flashcard.push_back({currQuestion, currAnswer});
+        }
+    }
+
+    std::cout << "Enter 'y' to revise the flashcards or 'back' to return." << "\n\n";
+    std::cout << "Enter command: ";
+    std::cin >> action;
+
+    while (true)
+    {
+        if (action == "y" || action == "back") break;
+        std::cout << "Invalid input! Try again.\n";
+        std::cout << "Enter command: ";
+        std::cin >> action;
+    }
+    std::cout << "\n";
+
+    if (action == "y") beginRevision();
+}
+
+void Flashcards::beginRevision()
+{
+    return; // NOT YET IMPLEMENTED
 }
 
 // Main
@@ -108,6 +144,7 @@ int main()
     Flashcards app;
 
     while (true) app.welcome(); // Calls welcome() and uses while loop to prevent adding additional frames to call stack
+
 
     return 0;
 }
